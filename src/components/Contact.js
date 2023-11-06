75983f2c2726462083227639ce364858b96b386f
Advaith Arun <advaith44@gmail.com>
2023-11-06 20:27:38 +0400

    Frontend of Contact Us page.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import ContactCSS from './StyleSheets/Contact.module.css';
 
 function Contact() {
     return (
         <div>
             <Header />
-            <div className="container mt-4">
-                <h1>Contact Us</h1>
-                <div className="row">
-                    <div className="col-md-6">
-                        <h2>Contact Information</h2>
-                        <p>Phone: +1-123-456-7890</p>
-                        <p>Email: contact@carhub.com</p>
-                        <p>Address: 123 Main Street, City, Country</p>
+            <div className={ContactCSS.BackGround}>
+                <div class="row">
+                    <div className={ContactCSS.squareBox}>
+                        <div className='container text-center p-5'>
+                            <h1 className='display-3 pb-3'>Contact Us</h1>
+                            <h2 className='h3 pb-2'>Contact Information</h2>
+                            <div className="py-1"><FontAwesomeIcon icon={faPhone} />&nbsp;&nbsp;&nbsp;+91 1234567890</div>
+                            <div className="py-1"><FontAwesomeIcon icon={faEnvelope} />&nbsp;&nbsp;&nbsp;<a href='mailto:' className='text-decoration-none text-white'>car360@email.com</a></div>
+                            <div className="py-1"><FontAwesomeIcon icon={faAddressCard} />&nbsp;&nbsp;&nbsp;123 Main Street, City, Country</div>
+                        </div>
+                    </div>
+
+
+                    <div className={ContactCSS.squareBox}>
+                        <div className='container p-5'>
+                            <form>
+                                <h1 className='display-3 pb-4 text-center'>Message Us</h1>
+                                <div class="form-outline mb-4">
+                                    <input type="text" id="formIn1" class="form-control" />
+                                    <label class="form-label" for="formIn1">Name</label>
+                                </div>
+                                <div class="form-outline mb-4">
+                                    <input type="email" id="formIn2" class="form-control" />
+                                    <label class="form-label" for="formIn2">Email address</label>
+                                </div>
+                                <div class="form-outline mb-4">
+                                    <input type="phone" id="formIn3" class="form-control" />
+                                    <label class="form-label" for="formIn3">Phone no.</label>
+                                </div>
+                                <div class="form-outline mb-4">
+                                    <textarea class="form-control" id="formIn4" rows="4"></textarea>
+                                    <label class="form-label" for="formIn4">Message</label>
+                                </div>
+                                <button type="submit" class="btn btn-outline-light btn-block mb-4 w-100" >Submit</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,30 +5,30 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <div className='footer p-3 bg-dark text-white'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-lg-5 col-md-12 d-flex flex-column justify-content-center align-items-start gap-1 pb-4'>
-                        <p className="py-1 fw-bold lead">CAR360</p>
-                        <div className="py-1"><FontAwesomeIcon icon={faPhone} />&nbsp;&nbsp;&nbsp;+91 1234567890</div>
-                        <div className="py-1"><FontAwesomeIcon icon={faEnvelope} />&nbsp;&nbsp;&nbsp;<a href='mailto:' className='text-decoration-none text-white'>car360@email.com</a></div>
-                    </div>
-                    <div className='col-lg-4 col-md-12 d-flex flex-column justify-content-center align-items-start gap-1 pb-4'>
-                        <h5>Useful Links</h5>
-                        <Link to="/" className='nav-link text-secondary py-1'>Home</Link>
-                        <Link to="/cars" className='nav-link text-secondary py-1'>Browse Cars</Link>
-                        <Link to="/contact" className='nav-link text-secondary py-1'>Contact Us</Link>
-                    </div>            
-                    <div className='col-lg-3 col-md-12 d-flex flex-column justify-content-center align-items-start gap-2 pb-4'>
-                        <h5>Subscribe</h5>
-                        <form className='d-flex flex-column justify-content-center align-items-start gap-2'>
-                            <input className='form-control py-1' placeholder='Your email' type='email' />
-                            <button className='btn btn-primary w-100 py-1' type='submit'>Subscribe</button>
-                        </form>
+            <div className='footer p-3 bg-dark text-white'>
+                <div className='container-fuild p-3'>
+                    <div className='row'>
+                        <div className='col-lg-4 col-md-12 d-flex flex-column justify-content-center align-items-start gap-1 pb-4'>
+                            <p className="py-1 fw-bold lead">CAR360</p>
+                            <div className="py-1"><FontAwesomeIcon icon={faPhone} />&nbsp;&nbsp;&nbsp;+91 1234567890</div>
+                            <div className="py-1"><FontAwesomeIcon icon={faEnvelope} />&nbsp;&nbsp;&nbsp;<a href='mailto:' className='text-decoration-none text-white'>car360@email.com</a></div>
+                        </div>
+                        <div className='col-lg-4 col-md-12 d-flex flex-column justify-content-center align-items-lg-center gap-1 pb-4'>
+                            <h5>Useful Links</h5>
+                            <Link to="/" className='nav-link text-secondary py-1'>Home</Link>
+                            <Link to="/cars" className='nav-link text-secondary py-1'>Browse Cars</Link>
+                            <Link to="/contact" className='nav-link text-secondary py-1'>Contact Us</Link>
+                        </div>            
+                        <div className='col-lg-4 col-md-12 d-flex flex-column justify-content-center align-items-start gap-2 pb-4 align-items-lg-end pe-4'>
+                            <h5 style={{paddingRight:60}}>Subscribe</h5>
+                            <form className='d-flex flex-column justify-content-center align-items-center gap-2'>
+                                <input className='form-control py-1' placeholder='Your email' type='email' />
+                                <button className='btn btn-primary w-100 py-1' type='submit'>Subscribe</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
