3f96e0bb71b5cd83f5204ec2ffd8e537876cf8c7
kevinseban <90603683+kevinseban@users.noreply.github.com>
2023-11-07 23:20:23 +0530

    img-slider

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -3,24 +3,21 @@ import { useParams } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import { getCarById } from './carsData';
+import ImageSlider from './ImageSlider'
 
 function CarDetail() {
   const { id } = useParams();
 
   const car = getCarById(id);
 
-  if (!car) {
-    return <div>Car not found</div>;
-  }
-
-  const carImageSrc = require(`./cars/${car.src}`);
-
   return (
     <div className='parent'>
       <Header />
       <div className='content bg-dark text-white p-4 overflow-hidden'>
         <h2>{car.name}</h2>
-        <img src={carImageSrc} alt={car.name} className='car-detail-img'/>
+        <div className='img-slider-container'>
+          <ImageSlider images={car.colors[0].images} />
+        </div>
         <p>Price: {car.price}</p>
       </div>
       <Footer />
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -2,35 +2,58 @@ import { ArrowBigLeft, ArrowBigRight } from 'lucide-react';
 import React, { useState } from 'react';
 
 function ImageSlider(props) {
-
     const [imageIndex, setImageIndex] = useState(0);
 
     function showNextImage() {
-        setImageIndex(index => {
-            if (index === props.images.length - 1) return 0
-            return index + 1
-        })
+        setImageIndex((index) => {
+            if (index === props.images.length - 1) return 0;
+            return index + 1;
+        });
     }
+
     function showPreviousImage() {
-        setImageIndex(index => {
-            if (index === 0) return props.images.length - 1
-            return index - 1
-        })
+        setImageIndex((index) => {
+            if (index === 0) return props.images.length - 1;
+            return index - 1;
+        });
     }
+
+    const requireImage = (path) => {
+        try {
+            return require(`./cars/${path}`);
+        } catch (error) {
+            console.error(error);
+            return '';
+        }
+    };
 
     return (
-        <div style={{ width: "100%", height: "100%", position: "relative" }}>
-            <div style={{ width: "100%", height: "100%", overflow: " hidden", display: "flex" }}>
-                {props.images.map((image, index) => (
-                    <img className='img-slider-img' key={index} src={image.src} alt={image.alt} style={{ transform: `translateX(${-100 * imageIndex}%)` }}
-                    />
-                ))}
+        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+            <div style={{ width: '100%', height: '100%', overflow: 'hidden', display: 'flex' }}>
+                {props.images.map((imagePath, index) => {
+                    const image = requireImage(imagePath);
+                    if (image) {
+                        return (
+                            <img
+                                className='img-slider-img'
+                                key={index}
+                                src={image}
+                                alt={`Car ${index+1}`}
+                                style={{ transform: `translateX(${-100 * imageIndex}%)` }}
+                            />
+                        );
+                    }
+                    return null;
+                })}
             </div>
-            <button onClick={showPreviousImage} className='img-slider-btn' style={{ left: "0" }}><ArrowBigLeft /></button>
-            <button onClick={showNextImage} className='img-slider-btn' style={{ right: "0" }}><ArrowBigRight /></button>
+            <button onClick={showPreviousImage} className='img-slider-btn' style={{ left: '0' }}>
+                <ArrowBigLeft />
+            </button>
+            <button onClick={showNextImage} className='img-slider-btn' style={{ right: '0' }}>
+                <ArrowBigRight />
+            </button>
         </div>
-    )
-
+    );
 }
 
 export default ImageSlider;
