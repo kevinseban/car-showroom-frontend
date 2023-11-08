0bbcd9299d0f034c3f5a9965d07949d124f8ad5c
Adarsh S <112859491+Adarsh887@users.noreply.github.com>
2023-11-08 20:16:01 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,39 @@
 import React, { useState , useEffect } from "react";
+import Header from "./Header";
+import axios from "axios";
+import { useNavigate ,Link } from "react-router-dom";
+
 
 export const Register = (props) => {
     const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const history=useNavigate();
+
+    async function submit(e){
         e.preventDefault();
-        console.log(email);
+
+        try{
+            await axios.post("http://localhost:8000/signup",{
+                email,password
+            })
+            .then(res=>{
+                if (res.data==="exist"){
+                    alert("User already exists")
+                }
+                else if (res.data==="notexist"){
+                    history("/home",{state:{id:email}})
+                    //history("/home")
+                }
+            })
+            .catch(e=>{
+                alert("Wrong details")
+                console.log(e);
+            })
+        }
+        catch(e){
+            console.log(e);
+        }
     }
 
     useEffect(() => {
@@ -15,19 +41,27 @@ export const Register = (props) => {
       });
 
     return (
-        <div className="auth-form-container log">
-            <h1 className="text-warning">Register</h1>
-        <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name" className="text-light">Full name</label>
-            <input className="form-control" value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email" className="text-light">Email</label>
-            <input className="form-control" value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password" className="text-light">Password</label>
-            <input className="form-control" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <br/>
-            <button className="btn btn-warning text-dark" type="submit">Log In</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+        <div>
+            <Header/>
+            <center>
+                <div className="auth-form-container log">
+                    <h1 className="text-warning">Sign Up</h1>
+                <form className="register-form" action="POST">
+                    <label htmlFor="email" className="text-light">Email</label>
+                    <input className="form-control" value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                    <label htmlFor="password" className="text-light">Password</label>
+                    <input className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                    <br/>
+                    <button className="btn btn-warning text-dark" type="submit" onClick={submit}>Sign up</button>
+                </form>
+                <br/>
+                <Link to="/">Already have an account? Login here.</Link>
+            </div>
+        </center>
     </div>
     )
 }
+
+/*
+<button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+*/
