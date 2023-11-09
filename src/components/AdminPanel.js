ca38fb981cf40d56c673e136d820b33800fb23c8
Advaith Arun <advaith44@gmail.com>
2023-11-09 16:23:19 +0400

    firebase-1

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { useState, useEffect } from "react";
+import { ref,uploadBytes,getDownloadURL,listAll,list,} from "firebase/storage";
+import { storage } from "./firebase";
+import { v4 } from "uuid";
 import Header from "./Header";
 import Footer from "./Footer";
 
 
 function AdminPanel() {
-  // const features = document.getElementById('feat').value.split('\n');
+  // const features = document.getElementById('feat').value.split('\n'); -> method to convert textarea features into array of features.
+
+  // Code to handle image upload to firebase
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const imagesListRef = ref(storage, "images/");
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+  //ending of code to send image to firebase
+
+
   return (
     <div className="parent">
       <Header />
@@ -56,6 +88,21 @@ function AdminPanel() {
               <label className='form-label' style={{fontSize:'18px',fontWeight:'600'}}><small>Features (newline seperated)</small></label>
               <textarea className='feat form-control' id='feat' cols="5" rows="5"/>
             </div>
+
+            {/* Images */}
+            <div className='form-group mb-3 mt-3'>
+              <label className='form-label' style={{fontSize:'18px',fontWeight:'600'}}>Pictures</label><br></br>
+              <input type="file" onChange={(event) => {
+                  setImageUpload(event.target.files[0]);
+                }}
+              />
+              <button onClick={uploadFile}>Upload Image</button><br />
+              {imageUrls.map((url) => {
+                  return <img src={url} />;
+              })}
+            </div>
+
+            {/* Submit Button */}
             <div className='container text-center'>
               <button type='submit' className='btn btn-success'>Submit</button>
             </div>
