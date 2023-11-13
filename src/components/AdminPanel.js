12c7eefdc3b6553045e73d943d725baa808d5b35
kevinseban <90603683+kevinseban@users.noreply.github.com>
2023-11-13 16:50:45 +0530

    .

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -306,7 +306,7 @@ function AdminPanel() {
                             <ul>
                               {color.images.map((image, index) => (
                                 <li key={index}>
-                                  <img src={image} alt={`Color ${color.name} - Image ${index}`} />
+                                  <img src={image} alt={`Color ${color.name} - Image ${index}`} style={{width:"300px"}} />
                                 </li>
                               ))}
                             </ul>
