169ee13753bcc07339747e6e14aa06093f33cf71
Advaith Arun <advaith44@gmail.com>
2023-11-15 20:46:33 +0400

    seacrh fucntion

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,10 +5,10 @@ import CarCard from './CarCard';
 import axios from 'axios';
 
 function CarList() {
-
   const [cars, setCars] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
   useEffect(() => {
-    // Fetch car data for all the cars in the server
     const fetchCarsData = async () => {
       try {
         const response = await axios.get('http://localhost:8000/cars/all');
@@ -20,14 +20,35 @@ function CarList() {
     fetchCarsData();
   }, []);
 
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/cars/search/${searchTerm}`);
+      setCars(response.data);
+    } catch (error) {
+      console.error('Error searching for cars:', error);
+    }
+  };
+
   return (
     <div className='parent'>
       <Header />
       <div className="content w-100 p-5 bg-dark text-white">
-        <h2 className='px-5 py-4'>Car List</h2>
+        <div className='container-fluid text-center'>
+          <h2 className='px-5 py-4'>Car List</h2><br></br>
+          <div className="search-bar px-5 mb-4">
+            <input
+              type="text"
+              style={{width:"300px"}}
+              placeholder="Search by model, make, etc."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            /><br /><br />
+            <button className='btn btn-primary' onClick={handleSearch}>Search</button>
+          </div>
+        </div>
         <div className='d-flex flex-wrap justify-content-center gap-5 px-3 pb-3'>
           {cars.map((car) => (
-          <CarCard key={car._id} carId={car._id}/>
+            <CarCard key={car._id} carId={car._id}/>
           ))}
         </div>
       </div>
